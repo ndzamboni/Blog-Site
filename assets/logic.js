cedd500586455b4f19d6dcbbc5fa7e7b22597fb9
ndzamboni <ndzamboni@gmail.com>
2024-04-19 22:38:25 -0400

    light/dark mode for landing page working

---
+++ assets/logic.js
@@ -0,0 +1,17 @@
+
+// javascript for light/mode and dark mode for landing page
+
+document.addEventListener('DOMContentLoaded', function () {
+    const checkbox = document.querySelector('input[type="checkbox"]');
+    
+    checkbox.addEventListener('change', function () {
+        if (this.checked) {
+            document.body.classList.add('dark-mode');
+        } else {
+            document.body.classList.remove('dark-mode');
+        }    
+    });
+});
+
+// ======================================================
+
